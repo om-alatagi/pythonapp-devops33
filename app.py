c6daa2702e5ade1b6d1229752fa5b65289a45ade
Trainersudhanshu <sudhanshutest123@gmail.com>
2025-04-27 12:02:53 +0530

    fixed linting

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)    
+    app = Flask(__name__)
+    
     @app.route('/')
     def home():
         print("inside home function")
