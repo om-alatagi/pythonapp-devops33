0cd061bcbb6f8e8d31f157b442d79682bdc30ec6
Trainersudhanshu <sudhanshutest123@gmail.com>
2025-04-27 13:33:59 +0530

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return '1234567890heyeyeyeyeyeyeGFG-33'
+        return 'bye1234567890heyeyeyeyeyeyeGFG-33'
 
     @app.route('/test')
     def test():
